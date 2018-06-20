368f6ffbf7909530fe23694fa994b2510e751bbd
hsiaosiyuan <hsiaosiyuan0@outlook.com>
2018-06-20 14:07:22 +0800

    deploy onto ropsten

--- truffle.js
+++ truffle.js
@@ -12,6 +12,11 @@
  *   },
  */
 
+const Web3 = require("web3");
+const web3 = new Web3();
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const mnemonic ="";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
@@ -20,6 +25,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
+      network_id: "3",
+      gas: 4600000,
+      gasPrice: web3.utils.toWei("20", "gwei")
     }
   }
 };
