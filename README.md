2df4d70122bdedaef899a4cfffb24480f9d07c42
hsiaosiyuan <hsiaosiyuan0@outlook.com>
2018-06-25 13:01:28 +0800

    deploy onto mainnet

---
+++ README.md
@@ -0,0 +1,3 @@
+# BDN Token Contracts
+
+ERC Token Standard #20 compatible token contracts of [BDN](https://bdn.network/index.html)
--- truffle.js
+++ truffle.js
@@ -15,7 +15,16 @@
 const Web3 = require("web3");
 const web3 = new Web3();
 const HDWalletProvider = require("truffle-hdwallet-provider");
-const mnemonic ="";
+const mnemonic = "";
+
+const WalletProvider = require("truffle-wallet-provider");
+const Wallet = require("ethereumjs-wallet");
+const mainNetPrivateKey = Buffer.from(process.env["MAINNET_PRIVATE_KEY"], "hex");
+const mainNetWallet = Wallet.fromPrivateKey(mainNetPrivateKey);
+const mainNetProvider = new WalletProvider(
+  mainNetWallet,
+  "https://mainnet.infura.io"
+);
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -27,10 +36,16 @@ module.exports = {
       network_id: "*" // Match any network id
     },
     ropsten: {
-      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/"),
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io"),
       network_id: "3",
       gas: 4600000,
       gasPrice: web3.utils.toWei("20", "gwei")
+    },
+    mainnet: {
+      provider: mainNetProvider,
+      gas: 4600000,
+      gasPrice: web3.utils.toWei("2", "gwei"),
+      network_id: "1"
     }
   }
 };
